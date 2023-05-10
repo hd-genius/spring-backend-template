85b682ffa0db214122383626cbcaeee68cc4f449
Roy Stewart <roythomasstewart@gmail.com>
2023-05-09 21:00:45 -0400

    Add a test example

---
+++ src/test/java/com/example/controllers/ExampleControllerTest.java
@@ -0,0 +1,11 @@
+package com.example.controllers;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ExampleControllerTest {
+    @Test
+    public void testExample() {
+        Assertions.assertTrue(true);
+    }
+}
